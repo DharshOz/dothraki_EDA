2e8ee3874db5a8123197af97964f669609dd7b2c
DharshOz <jayanthirajendran111@gmail.com>
2025-04-23 10:05:41 +0530

    first errorl

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-set -e  # Exit immediately if a command exits with a non-zero status
-
-echo "Installing dependencies..."
+set -e
 pip install --upgrade pip
 pip install -r requirements.txt
-
-echo "Creating static directory..."
-mkdir -p static
